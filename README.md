6adb2348bc01cd4da417e7b68b8c98b62fb03806
Neil <neil123@gmail.com>
2017-01-14 21:19:51 -0800

    Version 3 commited; the ugly code is banished

--- README.md
+++ README.md
@@ -11,18 +11,22 @@ http://nkpatel6.web.engr.illinois.edu/
 
 **Python:**Take the Fall15.csv file and strip out all class sections that don't give out actual grades (Discussions, Labs, recitals, lessons, etc.). Generate an output csv file with only data I actually care about, and an HTML file that stores everything in a basic, 1300 row, 4 column table.
 
-**HTML/CSS/JQUERY:** The final step is using a jQuery plugin called tablesorter that allowed me to turn my static HTML table into one that was sortable. A CSS style sheet completed the (albeit basic) look to allow for full functionality. The site is now hosted on my personal space at the University of Illinois.
+**HTML/CSS/JQUERY:** The final step is using a jQuery plugin called tablesorter that allowed me to turn my static HTML table into one that was sortable. A CSS style sheet completed the (albeit basic) look to allow for full functionality. The site is now hosted on my personal space at the University of Illinois, linked above.
 
 
 ##Future Features
 
-Create a Searchbar to look for specific classes
+Create a Searchbar to look for specific classes (Please use Control+F for now)
 
-Click on class and see grade distributions by professor. 
-
-**NEXT IN LINE THIS IS IMPORTANT** Combine data from multiple semesters rather then just the one included rn
+Click on class and see grade distributions by professor.
 
 ##Changelog
+
+**1/14/17:** Complete overhaul of python script: Did it get faster? Not sure, but its definitely much prettier code that I'm happier with; the old version worked well but was doing lots of unnecessary things (writing to csv, doing things over and over statically rather then in a loop, etc.) Had a few problems, asked around + posted on Stack Overflow. One more overhaul will be done eventually to replace all lists with a dictionary for a much cleaner data cleaning process. 
+
+**1/11/17:** Removed HTML from the Repo because this is a python project first; the webpage is simply to display the data. Plus the webpage is linked at the top of this readme anyway!  
+
+**1/6/17:** Edited Python Script to allow for multiple semesters of data to be used for more accuracy and a more comprehensive course list. Script rewrite in progress; adding the multiple semesters feature was copied and pasted from my complete rewrite. 
 
 **1/5/17:**Each course number is now linked to the course explorer site so visitors can navigate there for more info
 
---
+++ Version3.py
@@ -0,0 +1,205 @@
+import csv
+from bs4 import BeautifulSoup
+import urllib.request
+import requests
+
+def k(a,b,c):
+    def _k(item):
+        return (item[a],item[b],item[c])
+    return _k
+
+
+classarray=[]
+listoffiles=['28-S-15.csv','30-F-15.csv','31-S-16.csv','33-F-16.csv']
+y=0
+for file in listoffiles:
+    x=0
+    with open(file, encoding='latin-1') as csv123file:
+        reader = csv.DictReader(csv123file)
+        for row in reader:
+            y=y+1
+            tobeappended=[]
+            if x==0:
+                x=x+1
+            elif row['A+']!='N/A':
+                topics=['Course Subject','Course Number', 'Course Title', 'A+','A','A-','B+','B','B-','C+','C','C-','D+','D','D-','F']
+                for item in topics:
+                    tobeappended.append(row[item])
+                tobeappended.append(file[3:7])
+                classarray.append(tobeappended)
+
+classarray=sorted(classarray, key=k(0,1,2))
+
+
+###CLASSARRAY IS NOW A SORTED LIST OF CLASSES
+itemx = ['','','',0,0,0,0,0,0,0,0,0,0,0,0,0,'']
+data=open('data.txt', 'w')
+newarray=[]
+for course,courseplus1 in zip(classarray,classarray[1:]):
+    if (course[0]==courseplus1[0] and course[1]==courseplus1[1] and course[2]==courseplus1[2]):
+        itemx[0]=course[0]
+        itemx[1]=course[1]
+        itemx[2]=course[2]
+        for number in range(3,16):
+            itemx[number]=itemx[number]+(int(course[number])+int(courseplus1[number]))
+        itemx[16]=course[16]
+    else:
+        newarray.append(itemx)
+        itemx = ['', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '']
+        itemx[0]=courseplus1[0]
+        itemx[1]=courseplus1[1]
+        itemx[2]=courseplus1[2]
+        for number in range(3,16):
+            itemx[number]=int(courseplus1[number])
+        itemx[16] = courseplus1[16]
+
+for item in newarray:
+    print(item)
+
+
+
+genedCodes=['ACP','HUM','SBS','CW','CNW']
+classesListedByGened=[]
+
+for code in genedCodes:
+    urlWrapper="""https://courses.illinois.edu/search?year=2017&term=spring&keyword=&keywordType=qs&instructor=&collegeCode=&subjectCode=&creditHour=&degreeAtt=&courseLevel=&genedCode1=%s&genedCode2=&genedCode3=&partOfTerm=&_online=on&_open=on&_evenings=on"""
+    url=urlWrapper % (code)
+    openUrl = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(openUrl,"lxml")
+    letters = soup.find_all('a')
+    links=[]
+    x=0
+    for letter in letters:
+        links.append(letter.get('href'))
+    classes=[]
+    for link in links:
+        if len(link)>10:
+            if link[8]=='s':
+                classes.append(link)
+    courses=[]
+    for clas in classes:
+        x=x+1
+        blank, search123, schedule, year, semester, department, classnumber = clas.split("/")
+        classnumber, junk = classnumber.split('?')
+        actualClass = department + classnumber
+        courses.append(actualClass)
+    classesListedByGened.append(courses)
+    print(x)
+
+for gened in classesListedByGened:
+    print(gened)
+
+
+indexhtml = open('index.html', 'w')
+acphtml = open('acp.html', 'w')
+humhtml = open('hum.html', 'w')
+ssbshtml = open('ssbs.html', 'w')
+cnwhtml = open('cnw.html', 'w')
+cwhtml = open('cw.html', 'w')
+indextraits=['Grade Distribution Course Explorer','Grade Distribution Course Explorer']
+acptraits=['ACP Classes Grade Distribution Explorer','ACP Classes Grade Distribution Explorer']
+humtraits=['Humanities and the Arts Grade Distribution Explorer','Humanities and the Arts  Distribution Explorer']
+cwtraits=['Cultural Western Grade Distribution Course Explorer','Cultural Western Distribution Course Explorer']
+cnwtraits=['Non-Western Distribution Course Explorer','Non-Western Distribution Course Explorer']
+ssbstraits=['Social/Behavioral Classes Grade Distribution Course Explorer','Social/Behavioral Distribution Explorer']
+listoftraits=[indextraits,acptraits,humtraits,ssbstraits,cwtraits,cnwtraits]
+listofwebpages=[indexhtml,acphtml,humhtml,ssbshtml,cwhtml,cnwhtml]
+
+htmlstarter="""<!doctype html>
+<html lang="en-US">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html">
+  <title>%s</title>
+  <meta name="author" content="Neil Patel">
+  <link rel="shortcut icon" href="http://d15dxvojnvxp1x.cloudfront.net/assets/favicon.ico">
+  <link rel="icon" href="http://d15dxvojnvxp1x.cloudfront.net/assets/favicon.ico">
+  <link rel="stylesheet" type="text/css" media="all" href="css/styles.css">
+  <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+  <script type="text/javascript" src="js/jquery.tablesorter.min.js"></script>
+</head>
+
+<body>
+<ul>
+    <li><a class="active" href="./index.html">All</a></li>
+    <li><a href="./acp.html">ACP</a></li>
+    <li><a href="./hum.html">Humanities and the Arts</a></li>
+    <li><a href="./ssbs.html">Social/Behavioral</a></li>
+    <li><a href="./cw.html">Western</a></li>
+    <li><a href="./cnw.html">Non-Western</a></li>
+</ul>
+ <div id="wrapper">
+  <h1>%s</h1>
+
+  <table id="keywords" cellspacing="0" cellpadding="0">
+    <thead>
+      <tr>
+        <th><span>Class</span></th>
+        <th><span>Class Title</span></th>
+        <th><span>Total Students</span></th>
+        <th><span>Credit Hours</span></th>
+        <th><span>Percent A+/A</span></th>
+        <th><span>Percent A+/A/A-</span></th>
+      </tr>
+    </thead>
+    <tbody>"""
+
+x=0
+for page in listofwebpages:
+    text=htmlstarter %(listoftraits[x][0],listoftraits[x][1])
+    page.write(text)
+    x=x+1
+htmlwrapper = """ <tr>
+        <td class="lalign"><a href= "%s">%s</a></td>
+        <td>%s</td>
+        <td>%d</td>
+        <td>%s</td>
+        <td>%f</td>
+        <td>%f</td>
+      </tr>"""
+
+for course in newarray:
+    whichcourse = course[0] + course[1]
+    print(whichcourse)
+    totalFour=course[3]+course[4]
+    totalA=course[3]+course[4]+course[5]
+    totalKids=course[3]+course[4]+course[5]+course[6]+course[7]+course[8]+course[9]+course[10]+course[11]+course[12]+course[13]+course[14]+course[15]
+    percent4=totalFour/totalKids
+    percentA=totalA/totalKids
+    year = '20' + course[16][2] + course[16][3]
+    if course[16][0] == 'F':
+        actuallink = 'http://courses.illinois.edu/cisapp/explorer/schedule/' + year + '/fall/' + course[0] + '/' + \
+                     course[1] + '.xml?mode=cascade'
+    else:
+        actuallink = 'http://courses.illinois.edu/cisapp/explorer/schedule/' + year + '/spring/' + course[0] + '/' + \
+                     course[1] + '.xml?mode=cascade'
+    courseLink = 'https://courses.illinois.edu/schedule/2017/spring/' + course[0] + '/' + course[1]
+    credithoururl = urllib.request.urlopen(actuallink).read()
+    creditsoup = BeautifulSoup(credithoururl, "lxml")
+    letters = creditsoup.find_all('credithours')
+    letter = letters[0]
+    stringified = str(letter)
+    credithours = stringified[13]
+    tableBlob = htmlwrapper % (courseLink, whichcourse,course[2], totalKids, credithours, percent4, percentA)
+    indexhtml.write(tableBlob)
+    #ACP
+    x=0
+    for page in listofwebpages[1:]:
+        for item in classesListedByGened[x]:
+            if whichcourse==item:
+                page.write(tableBlob)
+        x=x+1
+
+
+
+for page in listofwebpages:
+    page.write("""    </tbody>
+      </table>
+     </div>
+    <script type="text/javascript">
+    $(function(){
+      $('#keywords').tablesorter();
+    });
+    </script>
+    </body>
+    </html>""")
